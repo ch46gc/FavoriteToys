6e2a3fc0bdd641b66a4379ff69ba3eb6797da724
ch46gc <chriseyhillgen@gmail.com>
2018-07-08 12:47:47 -0400

    Add ID to textView in layout and append string array to textView

--- app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.android.favoritetoys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    /*Declare TextView variable*/
+    TextView  mToysListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /* Use findViewById to get a reference to the TextView from the layout*/
+        mToysListTextView = findViewById(R.id.tv_toys_names);
+        /*Use the static ToyBox.getToyNames method and store the names in a String array*/
+        String[] toyNames = ToyBox.getToyNames();
+        /* Loop through each toy and append the name to the TextView (add \n for spacing)*/
+        for (String toyName : toyNames) {
+            mToysListTextView.append(toyName + "\n\n\n");
+
+        }
     }
 }
